ca4ebe942d1eb5e12e5a81b1c59e187c145dfe7f
daniel <dvds1987@gmail.com>
2022-03-22 17:11:03 +0100

    Change firebase account

--- models/client.js
+++ models/client.js
@@ -2,48 +2,47 @@ const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema;
 
 const ClientSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            trim: true,
-            required: true,
-        },
-        slug: {
-            type: String,
-            unique: true,
-            lowercase: true,
-            index: true,
-          },
-        email: {
-            type: String,
-            trim: true,
-            required: true,
-        },
-        phone: { 
-            type: Number, 
-            trim: true,
-            required: true 
-        },
-        event: { 
-            type: ObjectId,
-            ref: "Event"
-        },
-        eventName: { 
-            type: String,
-        },
-        price: { 
-            type: Number
-        },
-        message: {
-            type: String,
-            trim: true,
-        },
-        status: {
-            type: String,
-            default: "Niezatwierdzony",
-        },
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
     },
-    { timestamps: true }
+    slug: {
+      type: String,
+      lowercase: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      trim: true,
+      required: true,
+    },
+    event: {
+      type: ObjectId,
+      ref: "Event",
+    },
+    eventName: {
+      type: String,
+    },
+    price: {
+      type: Number,
+    },
+    message: {
+      type: String,
+      trim: true,
+    },
+    status: {
+      type: String,
+      default: "Niezatwierdzony",
+    },
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Client", ClientSchema);
